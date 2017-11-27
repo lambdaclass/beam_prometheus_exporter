ee2b014fb77d1296be3b9193725bf96e0ebfbcd2
Facundo Olano <facundo.olano@gmail.com>
2017-11-27 16:35:48 -0300

    default port, no tls

--- src/beam_prometheus_app.erl
+++ src/beam_prometheus_app.erl
@@ -9,18 +9,11 @@
 
 start(_StartType, _StartArgs) ->
   Dispatch = [{'_', [{"/metrics/[:registry]", prometheus_cowboy2_handler, []}]}],
-  {ok, _} = cowboy:start_tls(my_http_listener,
-                             [{port, config(port)},
-                              {certfile, config(certfile)},
-                              {keyfile, config(keyfile)}],
-                             #{env => #{dispatch => Dispatch}}
-                            ),
+  Port = application:get_env(beam_prometheus, port, 9101),
+  {ok, _} = cowboy:start_clear(my_http_listener,
+                               [{port, Port}],
+                                #{env => #{dispatch => Dispatch}}),
   beam_prometheus_sup:start_link().
 
 stop(_State) ->
   ok.
-
-%% Internal functions
-config(Key) ->
-  {ok, Value} = application:get_env(example, Key),
-  Value.
